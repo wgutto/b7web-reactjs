5baaeae0f59e116b225afc39de6c4f37afcffb7a
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-01-23 21:26:03 -0300

    estudos

---
+++ modulo 2/aula001/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Rectangle, {Square, Circle} from '@/components/Geo'
+
+function Page() {
+  return (
+    <div>
+      <h1 className="bg-white p-4 text-black font-bold text-center underline">Olá, mundo</h1>
+    
+    <Square/>
+
+    <Circle/>
+
+    <Rectangle/>
+    </div>
+  )
+}
+
+export default Page
---
+++ modulo 2/aula001/src/components/Geo.tsx
@@ -0,0 +1,23 @@
+export const Square = () => {
+    return (
+        <div className="w-24 h-24 bg-violet-500 mt-4 text-center">
+            Olá
+        </div>
+    )
+}
+
+export const Circle = () => {
+    return (
+        <div className="w-24 h-24 bg-blue-500 mt-4 text-center rounded-full">
+            Olá
+        </div>
+    )
+}
+
+const Rectangle = () => {
+    return (
+        <div className="w-44 h-24 bg-red-500 mt-4 text-center">Olá</div>
+    )
+}
+
+export default Rectangle
