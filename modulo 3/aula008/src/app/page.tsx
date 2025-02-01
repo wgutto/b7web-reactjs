a1310928e6e0989d636915289157629f6b0327c6
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-01-31 22:45:06 -0300

    estudos

---
+++ modulo 3/aula008/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { CustomButton } from "./components/CustomButton"
+
+const Page = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <CustomButton/>
+    </div>
+  )
+}
+
+export default Page
