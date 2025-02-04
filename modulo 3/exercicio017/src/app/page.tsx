97f52109c45b3d5062636714a0d9df1ba4d9511f
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-02-04 20:52:55 -0300

    estudos

--- modulo 3/exercicio017/src/app/page.tsx
+++ modulo 3/exercicio017/src/app/page.tsx
@@ -13,6 +13,8 @@ const Page = () => {
   const openModal = (id: number) => {
     const photo = photoList.find(item => item.id === id)
 
+    console.log(id)
+
     if(photo) {
       setImageOfModal(photo.url)
       setShowModal(true)
@@ -24,11 +26,11 @@ const Page = () => {
   }
 
   return (
-    <div className="w-screen h-screen p-4">
+    <div className="p-4">
 
       <h1 className="text-3xl text-center font-bold py-6">Galeria de fotos</h1>
 
-      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {photoList.map(item => 
             <PhotoItem key={item.id} photo={item} onClick={() => openModal(item.id)}/>
           )}
--- modulo 3/exercicio017/src/components/Modal.tsx
+++ modulo 3/exercicio017/src/components/Modal.tsx
@@ -1,14 +1,17 @@
-import { photoType } from "@/types/photoType"
-
 type Props = {
-    image: photoType,
+    image: string,
     closeModal: () => void
 }
 
 export const Modal = ({image, closeModal}:Props) => {
     return (
-        <div className="w-screen h-screen">
-            <img src={`/assets/${image.url}`} alt="" />
+        <div>
+            <div onClick={closeModal} className="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center">
+                <img src={`/assets/${image}`} alt="" className="max-w-screen max-h-screen" />
+            </div>
+            <div onClick={closeModal} className="fixed top-5 right-5 text-2xl cursor-pointer">
+                X
+            </div>
         </div>
     )
 }
