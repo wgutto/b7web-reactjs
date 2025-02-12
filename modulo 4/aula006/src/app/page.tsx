68f149ba5c1ee34d5ffc4c8626fbe2cafa1c8ae2
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-02-12 19:54:35 -0300

    estudos

--- modulo 4/aula006/src/app/page.tsx
+++ modulo 4/aula006/src/app/page.tsx
@@ -1,6 +1,48 @@
 "use client"
 
+import { Item } from "@/types/Item"
+import { useState } from "react"
+
 const Page = () => {
+  const [list, setList] = useState<Item[]>([])
+
+  const addNewList = (text: string) => {
+    setList([...list, {
+      id: list.length,
+      text,
+      done: false
+    }])
+  }
+
+  const editItemText = (id: number, newText: string) => {
+    setList(
+      list.map(text => {
+        if(text.id === id) {
+          text.text = newText
+        } 
+        return text
+      })
+    )
+  }
+
+  const toggleItem = (id: number) => {
+    setList(
+      list.map(text => {
+        if(text.id === id) {
+          text.done = !text.done
+        }
+        return text
+      })
+    )
+  }
+
+  const removeItem = (id: number) => {
+    setList(
+      list.filter(item => {
+        item.id !== id
+      })
+    )
+  }
 
   return (
     <div>
