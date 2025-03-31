bb9c7cbd5924a613266dbd74b3a5ced5bd69939f
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-03-31 20:24:05 -0300

    estudos

---
+++ tanstack-query/2-instalando-e-configurando-provider/src/app/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p className="text-white">Olá</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ tanstack-query/2-instalando-e-configurando-provider/src/utils/providers.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode
+}
+export const Providers = ({children}: Props) => {
+    const queryClient = new QueryClient()
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
